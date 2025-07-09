75181ebcd16cba10d286b65fe4de7ed79de5b50a
Prince070703 <pkabc1738@gmail.com>
2025-07-09 12:39:47 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,17 @@
+#  numbers from user
+a = input('The first number is :' )
+a = int(a)
+b = input('The second number is :' )
+b = int(b)
+
+# operation store in a variable
+c = a+b
+d = a-b
+e = a*b
+f = a/b
+
+# print the result
+print('Addition:' , c)
+print('Subtraction:' , d)
+print('Multiplication:' , e)
+print('Division:' , f)
---
+++ Task2.py
@@ -0,0 +1,8 @@
+# user input of name 
+a = input('Enter your first name:')
+b = input('Enter your last name:')
+c = a +b
+
+
+#printing of greeting message
+print('Hello , ' , c , 'Welcome to the Python Program')
